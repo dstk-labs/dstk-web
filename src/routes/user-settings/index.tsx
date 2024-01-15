07757cb5528467572c37f245253f7c2c7f792f67
StephenODea54 <odeastephen1@gmail.com>
2024-01-15 15:20:55 -0500

    Add Wireframe for User Settings Page

--- src/routes/user-settings/index.tsx
+++ src/routes/user-settings/index.tsx
@@ -1,3 +1,22 @@
+import { UserNavigation } from '@/components/settings';
+import { BreadcrumbItem, Breadcrumbs } from '@/components/ui';
+
 export const UserSettings = () => {
-    return <div>Welcome to the User Settings Page!</div>;
+    return (
+        <div className='w-full flex flex-col gap-8'>
+            <header className='flex flex-col gap-3'>
+                <Breadcrumbs>
+                    <BreadcrumbItem href='/dashboard'>Dashboard</BreadcrumbItem>
+                    <BreadcrumbItem href='/dashboard/settings/user-settings'>
+                        Settings
+                    </BreadcrumbItem>
+                    <BreadcrumbItem href='/dashboard/settings/user-settings'>
+                        User Settings
+                    </BreadcrumbItem>
+                </Breadcrumbs>
+                <h1 className='text-2xl font-medium text-gray-700 sm:text-3xl'>User Settings</h1>
+            </header>
+            <UserNavigation />
+        </div>
+    );
 };
