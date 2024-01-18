a3b71fa7cb52b362b4b607fc4a6ba1e55d21529f
StephenODea54 <odeastephen1@gmail.com>
2024-01-18 14:33:43 -0500

    Add Teams Header Component

--- src/routes/teams/index.tsx
+++ src/routes/teams/index.tsx
@@ -1,1 +1,13 @@
-export const Teams = () => <div>Teams be here</div>;
+import { useNavigate } from 'react-router-dom';
+
+import { TeamsHeader } from './components';
+
+export const Teams = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div className='w-full flex flex-col gap-12'>
+            <TeamsHeader navigateFn={navigate} />
+        </div>
+    );
+};
