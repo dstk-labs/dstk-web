504b877ea25374c0298dde646a08da5bcc50e6d7
StephenODea54 <odeastephen1@gmail.com>
2024-01-19 12:33:24 -0500

    Add Team Member Form

--- src/routes/add-team-member/components/index.ts
+++ src/routes/add-team-member/components/index.ts
@@ -1,1 +1,2 @@
+export * from './AddTeamMemberForm';
 export * from './AddTeamMemberHeader';
--- src/routes/add-team-member/index.tsx
+++ src/routes/add-team-member/index.tsx
@@ -3,25 +3,28 @@ import { BarLoader } from 'react-spinners';
 
 import { useListTeams } from '@/hooks';
 
-import { AddTeamMemberHeader } from './components';
+import { AddTeamMemberForm, AddTeamMemberHeader } from './components';
+import { useListUsers } from './api';
 
 export const AddTeamMember = () => {
     const { teamId } = useParams();
 
-    const { data, loading, error } = useListTeams(teamId);
+    const { data: teamsData, loading: teamsLoading, error: teamsError } = useListTeams(teamId);
+    const { data: usersData, loading: usersLoading, error: usersError } = useListUsers();
 
-    if (error) {
+    if (teamsError || usersError) {
         return <p>Error loading team information.</p>;
     }
 
-    if (loading) {
+    if (teamsLoading || usersLoading) {
         return <BarLoader color='#2563eb' width='250px' />;
     }
 
-    if (data && data.listTeams) {
+    if (teamsData && teamsData.listTeams && usersData && usersData.listUsers) {
         return (
             <div className='w-full flex flex-col gap-8'>
-                <AddTeamMemberHeader team={data.listTeams[0]} />
+                <AddTeamMemberHeader team={teamsData.listTeams[0]} />
+                <AddTeamMemberForm users={usersData.listUsers} />
             </div>
         );
     } else {
