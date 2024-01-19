b8130497ee04a99ae38e27cabc8d4cc0ba77bef1
StephenODea54 <odeastephen1@gmail.com>
2024-01-19 12:06:48 -0500

    Add team member header component

--- src/routes/add-team-member/index.tsx
+++ src/routes/add-team-member/index.tsx
@@ -1,1 +1,30 @@
-export const AddTeamMember = () => <div>Add Team Member Here</div>;
+import { useParams } from 'react-router-dom';
+import { BarLoader } from 'react-spinners';
+
+import { useListTeams } from '@/hooks';
+
+import { AddTeamMemberHeader } from './components';
+
+export const AddTeamMember = () => {
+    const { teamId } = useParams();
+
+    const { data, loading, error } = useListTeams(teamId);
+
+    if (error) {
+        return <p>Error loading team information.</p>;
+    }
+
+    if (loading) {
+        return <BarLoader color='#2563eb' width='250px' />;
+    }
+
+    if (data && data.listTeams) {
+        return (
+            <div className='w-full flex flex-col gap-8'>
+                <AddTeamMemberHeader team={data.listTeams[0]} />
+            </div>
+        );
+    } else {
+        return null;
+    }
+};
