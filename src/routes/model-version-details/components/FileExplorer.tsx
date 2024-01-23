14f64a2a26d42083e9b40e4cfec273cf8e013ac4
StephenODea54 <odeastephen1@gmail.com>
2024-01-23 09:59:21 -0500

    Add util to differentiate between folders and files

--- src/routes/model-version-details/components/FileExplorer.tsx
+++ src/routes/model-version-details/components/FileExplorer.tsx
@@ -1,3 +1,5 @@
+import { DocumentIcon, FolderIcon } from '@heroicons/react/24/outline';
+
 import {
     Card,
     Table,
@@ -8,6 +10,7 @@ import {
     TableRow,
 } from '@/components/ui';
 import type { StorageProviderObjectList } from '@/types/StorageProvider';
+import { isDirectory } from '../lib';
 
 type FileExplorerProps = {
     objects: StorageProviderObjectList;
@@ -28,7 +31,13 @@ export const FileExplorer = ({ objects }: FileExplorerProps) => {
                 <TableBody>
                     {objects.listObjectsForModelVersion.edges.map((edge) => (
                         <TableRow key={edge.node.name}>
-                            <TableCell>Hi</TableCell>
+                            <TableCell>
+                                {isDirectory(edge.node.name) ? (
+                                    <FolderIcon className='w-5 h-5' />
+                                ) : (
+                                    <DocumentIcon className='w-5 h-5' />
+                                )}
+                            </TableCell>
                             <TableCell>{edge.node.name}</TableCell>
                             <TableCell>{edge.node.size}</TableCell>
                             <TableCell>{edge.node.lastModified}</TableCell>
---
+++ src/routes/model-version-details/lib/fileBrowser.ts
@@ -0,0 +1,1 @@
+export const isDirectory = (filePath: string): boolean => filePath.split('/').length > 1;
